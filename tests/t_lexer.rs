4a4e5dbffb29fab93083ea2ff0c4d155a974a1c4
Garden <gardendistrict0x0@outlook.com>
2025-06-08 12:41:50 +1000

    feat: add basic lexer tests

--- tests/t_lexer.rs
+++ tests/t_lexer.rs
@@ -1,23 +1,54 @@
 
 #[test]
-fn test_lex_simple() {
-    //let src = "let x = 5;";
-    //let lexed = lex(src);
+fn lex_let_stmt() {
+    use rotor::lexer::{lex, TokenKind};
 
-    //assert!(lexed.is_working());
-    //assert_eq!(lexed.tokens[0].get_debug_info().contains("Let"), true);
+    let input = "let x = 42;";
+    let lexed = lex(input);
+
+    assert_eq!(lexed.tokens.len(), 5);
+    assert_eq!(lexed.tokens[0].kind, TokenKind::Let);
+    assert_eq!(lexed.tokens[1].kind, TokenKind::Identifier);
+    assert_eq!(lexed.tokens[2].kind, TokenKind::Equal);
+    assert_eq!(lexed.tokens[3].kind, TokenKind::Integer);
+    assert_eq!(lexed.tokens[4].kind, TokenKind::Semicolon);
 }
 
 #[test]
-fn test_parentheses() {
-    //let src = "(a + b) {c - d} [e * f]";
-    //let lexed = lex(src);
+fn lex_function_call() {
+    use rotor::lexer::{lex, TokenKind};
 
-    //assert!(lexed.is_working());
-    //assert!(lexed.tokens.iter().any(|t| t.kind == TokenKind::LParen));
-    //assert!(lexed.tokens.iter().any(|t| t.kind == TokenKind::RParen));
-    //assert!(lexed.tokens.iter().any(|t| t.kind == TokenKind::LCurly));
-    //assert!(lexed.tokens.iter().any(|t| t.kind == TokenKind::RCurly));
-    //assert!(lexed.tokens.iter().any(|t| t.kind == TokenKind::LSquare));
-    //assert!(lexed.tokens.iter().any(|t| t.kind == TokenKind::RSquare));
+    let input = "foo(\"bar\");";
+    let lexed = lex(input);
+
+    assert_eq!(lexed.tokens.len(), 5);
+    assert_eq!(lexed.tokens[0].kind, TokenKind::Identifier);
+    assert_eq!(lexed.tokens[1].kind, TokenKind::LParen);
+    assert_eq!(lexed.tokens[2].kind, TokenKind::String);
+    assert_eq!(lexed.tokens[3].kind, TokenKind::RParen);
+    assert_eq!(lexed.tokens[4].kind, TokenKind::Semicolon);
+}
+
+#[test]
+fn lex_operators_and_brackets() {
+    use rotor::lexer::{lex, TokenKind};
+
+    let input = "(a + b) - [c * d] / {e % f};";
+    let lexed = lex(input);
+
+    assert_eq!(lexed.tokens.len(), 13);
+    assert_eq!(lexed.tokens[0].kind, TokenKind::LParen);
+    assert_eq!(lexed.tokens[1].kind, TokenKind::Identifier);
+    assert_eq!(lexed.tokens[2].kind, TokenKind::Plus);
+    assert_eq!(lexed.tokens[3].kind, TokenKind::Identifier);
+    assert_eq!(lexed.tokens[4].kind, TokenKind::RParen);
+    assert_eq!(lexed.tokens[5].kind, TokenKind::Minus);
+    assert_eq!(lexed.tokens[6].kind, TokenKind::LSquare);
+    assert_eq!(lexed.tokens[7].kind, TokenKind::Identifier);
+    assert_eq!(lexed.tokens[8].kind, TokenKind::Multiply);
+    assert_eq!(lexed.tokens[9].kind, TokenKind::Identifier);
+    assert_eq!(lexed.tokens[10].kind, TokenKind::RSquare);
+    assert_eq!(lexed.tokens[11].kind, TokenKind::Divide);
+    assert_eq!(lexed.tokens[12].kind, TokenKind::Semicolon);
+    
 }
